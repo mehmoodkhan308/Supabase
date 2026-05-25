308cf39879e2019228266a877db23dfc77e5fc05
kashifkhanzada65 <muhammadkashifkhanzada110@gmail.com>
2026-05-25 10:32:39 +0500

    supabase connect with javascript

---
+++ class1/app.js
@@ -0,0 +1,34 @@
+// const  { createClient } = supabase
+
+// // Create a single supabase client for interacting with your database
+// const PROJECT_URL = 'https://tehpiwjtskeumkqtxdbq.supabase.co'
+// const PUBLISH_KEY = 'sb_publishable_KYh_9G-48j1wAyfKKZhSFw_QoXY2SFz'
+// const supabaseClient = createClient(PROJECT_URL, PUBLISH_KEY)
+// console.log("object",supabaseClient);
+
+
+// let score = prompt("enter your number")
+// console.log(parseInt(score));
+
+// let avg = Math.ceil(score)
+// console.log(avg);  
+
+
+// let min = 7
+// let max = 20
+// const ranNum = Math.floor(Math.random() * (max - min + 1) + min)
+// console.log(ranNum);
+
+
+// const ranNum = Math.floor(Math.random()*6)+1
+// console.log(ranNum);
+
+// let score = "23.9999"
+
+// console.log(parseFloat(score));
+// console.log(parseInt(score));
+// console.log(Number(score));
+
+// let num = 3.56666;
+// num = num.toFixed(8)
+// console.log(num);
